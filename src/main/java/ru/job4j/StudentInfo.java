891e7490bda10e4f2a680b39c9dc1b1f74f86b10
L.Volokhova <lyubanya-21@mail.ru>
2022-06-21 18:51:23 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println ("Lyubov Volokhova");
-        System.out.println ("07.07.1986");
+        System.out.println("Lyubov Volokhova");
+        System.out.println("07.07.1986");
     }
 }
